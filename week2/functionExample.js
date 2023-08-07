265814ec0099e2a6dfbe17aff222fecf0a448186
degr8sid-code <hsabdullah0@gmail.com>
2023-08-07 08:10:07 -0600

    update repo

---
+++ week2/functionExample.js
@@ -0,0 +1,42 @@
+var userName = "my user name"
+
+function (userName)
+{
+    userName
+}
+
+/* var myGlobalVariable = 10
+console.log(output);
+function check() {
+    var output = ''
+    if (typeof myGlobalVariable != "undefined")
+    {
+        output += "myGlobal: " + myGlobalVariable;
+    }
+    console.log("output: " + output)
+
+}
+
+check();
+
+function myFunction () {
+    console.log("This is my function");
+}
+
+myFunction(); 
+
+function userInfo(user) {
+    return "username: " + user
+   } 
+  // logging to the console the return of our function.
+   console.log(userInfo("Anelisy"))  
+   
+  function userInfo(user) {
+    if(user.length % 2 === 0) {
+      return "username: " + user + " is even, with" + user.length + " letters."
+    } else {
+      return "username: " + user + " is odd, with " + user.length + " letters."
+    }
+   }
+ */  
+
